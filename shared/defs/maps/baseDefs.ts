7968f3b68551d52fc3885c85761de6563298e466
LockBorn1 <161664739+LockBorn1@users.noreply.github.com>
2025-03-05 13:03:06 -0500

    Update baseDefs.ts

--- shared/defs/maps/baseDefs.ts
+++ shared/defs/maps/baseDefs.ts
@@ -566,8 +566,8 @@ export const Main: MapDef = {
                 barn_01: { small: 1, large: 3 },
                 barn_02: 1,
                 hut_01: 3,
-                hut_02: 2, // spas hut
-                hut_03: 2, // scout hut
+                hut_02: 1, // spas hut
+                hut_03: 1, // scout hut
                 shack_03a: 2,
                 shack_03b: { small: 2, large: 3 },
                 greenhouse_01: 1,
